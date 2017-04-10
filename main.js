97557bb2ac7bacd015e13fe2c3a52caa48bf0bf6
shitkins <tomas@Tomass-MBP-3.lan>
2017-04-10 06:20:03 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+viewImgurAlbum = function(word){
+  var query = word.selectionText;
+  console.log(query);
+  chrome.windows.create({url: "http://" + query + ".imgur.com", incognito: true});
+};
+
+chrome.contextMenus.create({
+  title: "View Imgur Albums!",
+  contexts:["selection"],
+  onclick: viewImgurAlbum
+});
